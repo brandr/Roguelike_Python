78f9ce1a066de872bb613df4b8dd5fe809ed359f
brandr <magicalfarmrancherseries@yahoo.com>
2014-06-20 16:48:48 -0700

    pulling

--- effect.py
+++ effect.py
@@ -1,3 +1,18 @@
 """ A graphical effect occupying a single tile.
 """
 
+#TODO imports
+
+class Effect:
+	""" Effect( char, Color ) -> Effect
+
+	A visual effect occupying one tile that does not affect gameplay directly.
+
+	Attributes:
+
+	symbol: the symbol that represents this effect.
+
+	color: the color representing this effect.
+	"""
+	def __init__(self, symbol, color):
+		self.symbol, self.color
